73a55ae50355b4dcf59b03aea63de141666e7d59
Aravind <arvindarru57@gmail.com>
2024-09-12 10:33:26 +0530

    Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -387,4 +387,35 @@ WITH CTE AS (
 SELECT CASE WHEN DIFF > 7 THEN DATEADD(DAY, 7, SLS_DT) END AS MISSING_WK
 FROM CTE
 WHERE DIFF > 7;
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--WAQ TO GET THE COMPANY ID WHO HAVE ATLEAST 2 USERS WHO SPEAKS BOTH ENGLISH AND GERMAN
+CREATE TABLE Company_user (
+    Company_Id VARCHAR(512),
+    User_Id INT,
+    Language VARCHAR(512)
+);
 
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '1', 'German');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '1', 'English');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '2', 'German');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '3', 'English');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '3', 'German');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('1', '4', 'English');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '5', 'German');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '5', 'English');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '5', 'German');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '6', 'Spanish');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '6', 'English');
+INSERT INTO Company_user (Company_Id, User_Id, Language) VALUES ('2', '7', 'English');
+SELECT * FROM COMPANY_USER
+
+WITH CTE AS(
+              SELECT COMPANY_ID,USER_ID,COUNT(*) CT FROM COMPANY_USER
+              WHERE LANGUAGE IN ('ENGLISH','GERMAN')
+              GROUP BY COMPANY_ID,USER_ID
+              HAVING COUNT(*)>1
+         )
+
+SELECT COMPANY_ID,COUNT(*) CT
+FROM CTE GROUP BY COMPANY_ID
+HAVING COUNT(*)>1
